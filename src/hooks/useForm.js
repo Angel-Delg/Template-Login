83d12401b63f4bca20e732e1ed97f26cbb59dad8
Angel Delgado <lariosangel062@gmail.com>
2023-02-16 02:34:58 -0600

    Agregando custom hook useForm

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,3 +1,23 @@
-export const useForm = () => {
+import { useState } from "react"
+
+export const useForm = (initialForm = {}) => {
+
+   const [formState, setFormState] = useState( initialForm )
    
+   const onFormChage = ({target:{ value, name }}) =>{
+      setFormState({
+         ...formState,
+         [name]:value
+      })
+   }
+
+   const onResetForm = () => {
+      setFormState(initialForm)
+   }
+
+   return {
+      formState,
+      onFormChage,
+      onResetForm
+   }
 }
